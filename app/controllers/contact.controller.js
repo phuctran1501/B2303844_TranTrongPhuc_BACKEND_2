fdfeb02e4a1c844f4aa71115fc4d5b9a45fa2afd
Phuc <trantrongphuc1501@gmail.com>
2026-01-18 13:15:12 +0700

    Cai dat handler

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,3 +1,7 @@
+const ApiError = require("../api-error");
+const MongoDB = require("../utils/mongodb.util");
+const ContactService = require("../services/contact.service");
+
 exports.create = (req, res) => {
   res.send({ message: "create handler" });
 };
@@ -23,4 +27,19 @@ exports.deleteAll = (req, res) => {
 
 exports.findAllFavorite = (req, res) => {
   res.send({ message: "findAllFavorite handler" });
+};
+
+exports.create = async (req, res, next) => {
+  if (!req.body?.name) {
+    return next(new ApiError(400, "Name can not be empty"));
+  }
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const document = await contactService.create(req.body);
+    return res.send(document);
+  } catch (error) {
+    return next(
+      new ApiError(500, "An error occurred while creating the contact"),
+    );
+  }
 };
